3b5a735fd80daf00a4baf9d5095385d0695a390a
mitali <mitali.barhate0906@gmail.com>
2026-06-18 12:43:46 +0530

    db  commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Home Route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Book Management System API Running',
+  });
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
